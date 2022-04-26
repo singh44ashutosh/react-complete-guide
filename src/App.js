070dabc09a6e4fb2d0de2e55f61434889dcc449e
Ashutosh Kumar Singh <singh44ashutosh@gmail.com>
2022-04-26 18:09:51 +0530

    till filter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -28,12 +29,16 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,41 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const onFilterHandler = (value) => {
+    console.log(value);
+  };
+
+  return (
+    <div>
+      <ExpensesFilter onFilter={onFilterHandler} />
+      <Card className="expenses">
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
+        <ExpenseItem
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
+        />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
