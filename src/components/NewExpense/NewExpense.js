f507491245d69a666fc8ec48ae5d4eeb6857a270
Ashutosh Kumar Singh <singh44ashutosh@gmail.com>
2022-04-30 05:51:02 +0530

    Add New Button

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,38 @@
+import { useState } from "react";
+
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [formMode, setFormMode] = useState("VIEW");
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setFormMode("VIEW");
   };
+
+  const addNewExpenseHandler = () => setFormMode("ADD");
+
+  const cancelHandler = () => setFormMode("VIEW");
+
+  if (formMode === "VIEW") {
+    return (
+      <div className="new-expense">
+        <button onClick={addNewExpenseHandler}>Add New Expense</button>
+      </div>
+    );
+  }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      <ExpenseForm
+        onSaveExpenseData={saveExpenseDataHandler}
+        onCancel={cancelHandler}
+      />
     </div>
   );
 };
